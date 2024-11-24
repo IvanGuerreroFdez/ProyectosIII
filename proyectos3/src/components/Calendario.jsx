b86f370b4276e8a36358dfa20f1a5f815257a9f6
alexCaballero4 <alexcaballero846@gmail.com>
2024-11-24 13:24:39 +0100

    Update Calendario.jsx

--- proyectos3/src/components/Calendario.jsx
+++ proyectos3/src/components/Calendario.jsx
@@ -14,17 +14,19 @@ const Calendario = () => {
   }, []);
 
   const eventosEnFecha = eventosInscritos.filter((evento) => {
-    // Simularemos que todos los eventos tienen fecha de hoy por defecto
+    // Simulamos que todos los eventos tienen fecha de hoy por defecto
     return new Date(evento.fecha || new Date()).toDateString() === fechaSeleccionada.toDateString();
   });
 
   return (
     <div className="calendario-container">
       <h2>Mi Calendario de Actividades</h2>
-      <Calendar
-        onChange={setFechaSeleccionada}
-        value={fechaSeleccionada}
-      />
+      <div className="calendario">
+        <Calendar
+          onChange={setFechaSeleccionada}
+          value={fechaSeleccionada}
+        />
+      </div>
       <div className="eventos-en-fecha">
         <h3>Eventos en {fechaSeleccionada.toDateString()}</h3>
         {eventosEnFecha.length > 0 ? (
